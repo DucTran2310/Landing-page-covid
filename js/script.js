af313aef02562232e6b6d439533719101991a9f4
DucTran2310 <18520021@gm.uit.edu.vn>
2021-09-18 15:57:42 +0700

    Add animation handwash, doctor and scroll top

--- js/script.js
+++ js/script.js
@@ -64,6 +64,16 @@ counters.forEach(counter => {
 	updateCount();
 });
 
+// Scroll top
+const scrollTop = document.querySelector('.scroll-top');
+
+window.addEventListener("scroll", ()=> {
+    if(window.pageYOffset > 100) {
+        scrollTop.classList.add("active");
+    } else {
+        scrollTop.classList.remove("active");
+    }
+})
 
 
 
